5c2d91e872d9408691fb056f064c53f55f45d1d5
mahdi <71930380@students.liu.edu.lb>
2024-06-18 11:59:10 +0300

    desgin and get fake articles data from placeholder api

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import ArticleItem from "@/components/articles/ArticleItem";
 
-const ArticlesPage = () => {
-  return <div>ArticlesPage</div>;
+import { Article } from '@/utils/types'
+
+const ArticlesPage = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts: Article[] = await response.json();
+  let postsList = posts.map((p) => {
+    return (<ArticleItem article={p} key={p.id} />)
+  })
+
+  return <section className="container m-auto px-5">
+    <div className="flex items-center justify-center gap-7 flex-wrap" >{postsList}</div>
+
+  </section>;
 };
 
 export default ArticlesPage;
