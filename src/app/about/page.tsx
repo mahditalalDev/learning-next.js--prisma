b7681a9df494e73c19f044313c27b3c75bdd5bfe
mahdi <71930380@students.liu.edu.lb>
2024-06-19 13:26:02 +0300

    work with image components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import Image from "next/image";
+import CloudImage from "../../../public/cloudImage.png"
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return <section className="fix-height container m-auto" >
+    <h1 className="p-5 text-3xl font-bold text-gray-800" >About Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, hic?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minima.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nobis, non corporis a eaque cum?
+    </p>
+    <div>
+      <Image src={CloudImage} alt="cloud" />
+    </div>
+  </section>;
 };
 
 export default AboutPage;
